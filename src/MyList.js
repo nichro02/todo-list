f7f59a6ce32da38a182e98d400b8d596792248e2
nichro02 <nichro02@gmail.com>
2020-11-24 18:54:46 -0800

    Added form and button to enable adding items to list

--- src/MyList.js
+++ src/MyList.js
@@ -5,7 +5,24 @@ import './App.css'
 class MyList extends Component {
   
   state = {
-    toDoItemArray: this.props.theList
+    toDoItemArray: this.props.theList,
+    newItem: ''
+  }
+
+  newItemChange = (event) => {
+    console.log('NEW ITEM RECEIVED', event.target.value)
+    this.setState({
+      newItem: event.target.value
+    })
+  }
+
+  addItem = (event) => {
+    console.log('NEW ITEM ADDED TO ARRAY', this.state.newItem)
+    event.preventDefault()
+    this.setState({
+      toDoItemArray: [...this.state.toDoItemArray, this.state.newItem],
+      newItem: ''
+    })
   }
 
   clearList = () => {
@@ -27,6 +44,14 @@ class MyList extends Component {
           {todoItems}
         </ul>
         <button onClick={this.clearList}>Finished the list!</button>
+        <form>
+          <input type='text'
+            placeholder='Add an item here'
+            onChange={(event) => this.newItemChange(event)}
+            value={this.state.newItem}
+          />
+          <button onClick={(event) => this.addItem(event)}>Add to list</button>
+        </form>
       </div>
     )
   }
